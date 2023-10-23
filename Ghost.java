3322d6fe725a041c20f75a9ea2012d7e54b4bba2
KCalgo <kayra.calgara@gmail.com>
2023-10-23 12:12:37 +0200

    jkaegb

--- Ghost.java
+++ Ghost.java
@@ -22,8 +22,8 @@ public class Ghost extends Movables {
 
         x = (rowPos * Grid.TILE_SIZE) + (Grid.TILE_SIZE / 2);
         y = (colPos * Grid.TILE_SIZE) + (Grid.TILE_SIZE / 2);
-        if ((Grid.ROW_AMOUNT / 2.0) - 1 < rowPos || rowPos >  (Grid.ROW_AMOUNT / 2.0) + 1
-        && (Grid.COLUMN_AMOUNT / 2.0) - 1 < colPos || colPos >  (Grid.COLUMN_AMOUNT / 2.0) + 1) {
+        if ((Grid.ROW_AMOUNT / 2.0) - 2 < rowPos || rowPos >  (Grid.ROW_AMOUNT / 2.0) + 2
+        && (Grid.COLUMN_AMOUNT / 2.0) - 2 < colPos || colPos >  (Grid.COLUMN_AMOUNT / 2.0) + 2) {
             System.out.println("X:" + x + "Y:" + y);
         } else {
             spawn();
